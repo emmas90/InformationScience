8cf4536f34c4c726771b4fc51f10689b736d4191
emmas90 <emmasophianagels@gmail.com>
2020-11-25 21:20:53 +0100

    first draft assignment1

---
+++ assignments/ass1.py
@@ -0,0 +1,43 @@
+### Importing packages
+
+import urllib.request
+from json import dumps, loads  
+from urllib.parse import quote
+
+# Asking for user input
+object_search = input("Enter an object for your search: ")
+
+# Make sure the user input is a valid query string value
+safe_query = quote(object_search)
+
+webUrl = urllib.request.urlopen('https://www.europeana.eu/api/entities/suggest?wskey=apidemo&text=' + safe_query + '&type=agent')
+
+# load JSON from the URL into a variable
+data = loads(webUrl.read())
+
+# dump the JSON object as a string
+print(dumps(data))
+
+### Importing packages
+
+import urllib.request
+from json import dumps, loads  
+from urllib.parse import quote
+
+try:
+    # Asking for user input
+    object_search = input("Enter an object for your search: ")
+
+    # Make sure the user input is a valid query string value
+    safe_query = quote(object_search)
+
+    webUrl = urllib.request.urlopen('https://www.europeana.eu/api/entities/suggest?wskey=apidemo&text=' + safe_query + '&type=agent')                                 
+
+    # load JSON from the URL into a variable
+    data = loads(webUrl.read())
+
+    # dump the JSON object as a string
+    print(dumps(data))
+    
+except Exception as e:
+    print("Hey hey foute boel, iets ging mis.")
