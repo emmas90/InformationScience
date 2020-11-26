7c6e6a12850d8a3bfca78a99ee88cb7a47713951
emmas90 <emmasophianagels@gmail.com>
2020-11-26 22:33:55 +0100

    delete files we dont need

--- assignments/ass1.py
+++ assignments/ass1.py
@@ -1,22 +1,3 @@
-### Importing packages
-
-import urllib.request
-from json import dumps, loads  
-from urllib.parse import quote
-
-# Asking for user input
-object_search = input("Enter an object for your search: ")
-
-# Make sure the user input is a valid query string value
-safe_query = quote(object_search)
-
-webUrl = urllib.request.urlopen('https://www.europeana.eu/api/entities/suggest?wskey=apidemo&text=' + safe_query + '&type=agent')
-
-# load JSON from the URL into a variable
-data = loads(webUrl.read())
-
-# dump the JSON object as a string
-print(dumps(data))
 
 ### Importing packages
 
